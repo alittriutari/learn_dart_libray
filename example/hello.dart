8be7feb1d917597502518af34b401e1f54c9f26d
alittriutari <alittriutari@gmail.com>
2023-07-27 11:04:38 +0800

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,12 @@
+//buat nyoba file lib yg dibuat td
+
+//import package
+import 'package:learn_dart_library/hello.dart';
+
+void main() {
+  print(sayHello('alit'));
+
+  var customer = Customer();
+  var category = Category();
+  // var product = Product(); --> gabisa dipake karna ga di show
+}
---
+++ lib/hello.dart
@@ -0,0 +1,5 @@
+library learn_dart_library;
+
+export 'src/customer.dart'
+    show Customer, Category; //export cuma class Customer aja
+export 'src/say_hello.dart'; //export semua yg ada di dalem sayHello
